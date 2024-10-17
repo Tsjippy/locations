8d8b390d832f8e8f046465a4e06414ef1ed79e11
Tsjippy <ewaldha@hotmail.com>
2024-10-17 12:18:52 +0100

    updated: blocks

--- blocks/metadata/build/index.asset.php
+++ blocks/metadata/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '754032bd0801ae26f435');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '8e59b3236fa5e54e32e6');
