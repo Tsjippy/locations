9b7df65068fd9c7095fe15912e4d7aba93342c6e
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:49 +0200

    changed: removed 'tested up to'

--- tsjippy-locations.php
+++ tsjippy-locations.php
@@ -10,7 +10,6 @@ namespace TSJIPPY\LOCATIONS;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
  * Plugin URI:            https://github.com/Tsjippy/
  * Tested:               7.1
  * TextDomain:            tsjippy
